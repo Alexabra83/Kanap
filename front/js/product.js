f34c65a80eb77d9ff0b4e960306b500451e80f54
Alexabra83 <alexabra83@gmail.com>
2022-12-28 08:41:35 +0100

    creat addToBasket

--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,8 @@
 // VARIABLES et CONSTANTES
 // récupérer l'id dans l'url de la page, ajouter dans l'APIURL
 const API_URL = "http://localhost:3000/api/products/";
-
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
 
 // FONCTIONS
 
@@ -13,7 +14,7 @@ const API_URL = "http://localhost:3000/api/products/";
 function getKanap(){
     fetch(API_URL + id)
     .then(res => res.json())
-    .then(data => displayKanap(kanap));
+    .then(data => displayKanap(data));
 }
 
 /**
@@ -22,26 +23,87 @@ function getKanap(){
  */
 function displayKanap(kanap){
 
-        let productLink = document.createElement("a");
-        productLink.href = "./product.html?id=" + kanap._id
-        document.getElementById("items").append(productLink);
-
-        let productArticle = document.createElement("article");
-        productLink.appendChild(productArticle);
-
         let productImg = document.createElement('img');
         productImg.src = kanap.imageUrl;
         productImg.alt = kanap.altTxt;
-        productArticle.appendChild(productImg);
+        let divItemImg = document.querySelector('.item__img');
+        divItemImg.appendChild(productImg);
     
-        let productName = document.createElement('h3');
+        let productName = document.getElementById('title');
         productName.innerText = kanap.name;
-        productArticle.appendChild(productName);
 
-        let productDescription = document.createElement('p');
+        let productPrice = document.getElementById('price');
+        productPrice.innerText = kanap.price;
+
+        let productDescription = document.getElementById('description');
         productDescription.innerText = kanap.description;
-        productArticle.appendChild(productDescription);
+
+        getAllColors(kanap.colors)
 }
+
+function getAllColors(colors) {
+    let selectColor = document.getElementById("colors");
+    for (let i = 0; i < colors.length; i++) {
+        let optionColor = document.createElement("option");
+        optionColor.value = colors[i];
+        optionColor.innerText = colors[i];
+        selectColor.appendChild(optionColor);
+    }
+}
+
+// addEventListener
+/**
+    addToBasket.addEventListener("click", (event) => {
+    event.addToBasket();
+    */
+    
+    function saveBasket(kanap){
+        localStorage.setItem("basket", JSON.stringify(basket));
+    }
+
+    function getBasket(){
+        let basket = localStorage.getItem("basket");
+        if (basket == null){
+            return[];
+        }else{
+            return JSON.parse(basket);
+        }
+    }
+
+    function addToBasket(kanap) {
+        let basket = getBasket();
+        let foundKanap = basket.
+        basket.push(kanap);
+        saveBasket(basket);
+    }
+
+
+
+
+
+
+
+
+
+
+
+        /**let basket = localStorage.getItem("product")
+        //basket = JSON.parse(basket)
+    
+        //if(basket){
+    
+        }
+        //else{
+            basket = [];
+            basket.push()
+        }
+    }*/
+
+//fin addEventListener
+/** })*/
+
+
+
 
 // Crée une fonction add too basket qui sera appelé par add event listener
 // coupler la quantité des éléments identiques et pas dans cart.js (bonne pratique)
