d2679b2440e9bdeec9a75da45440e04545dbe5f0
Alexabra83 <alexabra83@gmail.com>
2023-01-09 10:42:39 +0100

    intro cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -11,8 +11,36 @@ let id = params.get("id");
 /**
  * GET KANAP
  */
-function getKanap(){
+function getBasket(){
     fetch(API_URL + id)
     .then(res => res.json())
-    .then(data => displayKanap(data));
+    .then(data => displayBasket(data));
+}
+
+
+function getBasket() {
+    let cart = localStorage.getItem("basket");
+
+    return JSON.parse(cart);
+}
+
+
+/** information kanap panier */
+function displayKanap(kanap){
+
+    /**cart item */
+    let productImg = document.createElement('cart__img');
+    productImg.src = kanap.imageUrl;
+    productImg.alt = kanap.altTxt;
+    let divItemImg = document.querySelector('.cart__item__img');
+    divItemImg.appendChild(productImg);
+
+    /**cart item content */
+    let productName = document.getElementById('title');
+    productName.innerText = kanap.name;
+    
+    /**cart content descrption */
+    let productDescription = document.getElementById('description');
+    productDescription.innerText = kanap.description;
+
 }
